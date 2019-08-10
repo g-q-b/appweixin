b73e7096fc23ce2667b4f0de62c0a7ec4bd01b08
g-q-b <1105503866@qq.com>
2019-08-10 20:13:45 +0800

    第一次

---
+++ API/routes/api/nav.js
@@ -0,0 +1,45 @@
+let express = require('express')
+let router = express.Router()
+
+let mgdb = require('../../utils/mgdb')
+
+router.get('/',(req,res,next)=>{
+  console.log('banner')
+  mgdb({
+    url:'mongodb://127.0.0.1:27017',
+    dbName:'gao',
+    collectionName:'nav'
+  },(collection)=>{
+    collection.find({
+
+    },{
+    }).toArray((err,result)=>{
+      console.log('err',err)
+      if(!err){
+        res.send({err:1,msg:'成功',data:result})
+      }
+    })
+  })
+})
+router.get('/:_id',(req,res,next)=>{
+  console.log(req.params)
+  let _id = req.params._id;
+  mgdb({
+    url:'mongodb://127.0.0.1:27017',
+    dbName:'local',
+    collectionName:'gao'
+  },(collection,client,ObjectID)=>{
+    collection.find({
+      _id:ObjectID(_id)
+    }).toArray((err,result)=>{
+      console.log('result',result)
+      if(!err){
+        res.send({err:0,msg:'查询成功',data:result[0]})
+      }
+    })
+  })
+})
+
+
+
+module.exports = router;
